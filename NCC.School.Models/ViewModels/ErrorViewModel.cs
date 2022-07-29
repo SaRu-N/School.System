6508c186be823f27d6f70aeaa443bf80432e9f82
SaRu-N <saruniroula73@gmail.com>
2022-07-30 04:15:04 +0545

    project Restructure

---
+++ NCC.School.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NCC.School.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
